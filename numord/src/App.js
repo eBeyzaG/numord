9675cb641ce1a499217d8a761e10bb8e62417aa9
Beyza <beyzagurer@hotmail.com>
2023-02-24 14:30:47 +0300

    Add privacy policy

--- numord/src/App.js
+++ numord/src/App.js
@@ -1,3 +1,4 @@
+
 import { Play } from "./containers/Play";
 
 function App() {
@@ -5,16 +6,43 @@ function App() {
   return (
 
     <div className="App">
-        <div className="container main">
-        <div style={{textAlign: "center"}}>
+        <div className="container main" style={{textAlign: "center", width: "60%"}}>
+        <div >
             <h1>numord</h1>
         </div>
+      
+        <br></br>
+        <h3>Gizlilik Sözleşmesi</h3>
+        <p>
+        <br></br>
+                Son Güncelleme 24/02/2023
+                <br></br>
+
+
+                Bu sözleşme, numord uygulaması için ben, Beyza Gürer, tarafından hazırlanmış olup gizlilik detaylarını içermektedir.
+              <br></br>
+              <br></br>
+                numord uygulaması kullanıcıdan herhangi bir kişisel veri toplamaz.
+                <br></br>
+                <br></br>
+
+                Mail aracılığıyla bana ulaşan kullanıcılar, bunu kendi istekleri dahilinde yapar ve kendi istedikleri verileri paylaşır. Mail yoluyla paylaştığınız verileri kendi riskiniz dahilinde olduğunu unutmayınız. Mail yoluyla paylaşılan veriler herhangi bir üçüncü parti uygulama ile paylaşılmaz.
+                <br></br>
+                <br></br>
+                Sözleşme herhangi bir zamanda güncellenebilir, güncellemeleri kontrol etme sorumluluğu kullanıcıya aittir.
+<br></br>
+<br></br>
+                İletişim için: numord264351@gmail.com
+
+        </p>
         <div > 
             <a href='mailto:numord264351@gmail.com'>
                 <h2>Bize Ulaşın</h2>
-                </a>
+            </a>
         </div>
         </div>
+      
+       
     </div>
   );
 }
